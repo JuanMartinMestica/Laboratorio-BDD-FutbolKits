2dcb4e18c5bdde7681d8ddb1245d9e82fcb11069
Juan Martín Mestica <juan.mestica@est.fi.uncoma.edu.ar>
2024-05-09 14:06:21 -0300

    Se agregan scripts de base de datos

---
+++ create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE producto (
+    nombre VARCHAR(100) PRIMARY KEY,
+    temporada VARCHAR(50),
+    link_foto TEXT,
+    continente VARCHAR(50),
+    equipo VARCHAR(100),
+    liga VARCHAR(100),
+    tipo VARCHAR(50)
+);
